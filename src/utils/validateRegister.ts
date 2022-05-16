dba7d6f75aae0b87b40dc56e6e5d33cf09165389
Youngjun <yjl.2000@hotmail.com>
2022-05-16 00:03:55 -0400

    created validation for registration

---
+++ src/utils/validateRegister.ts
@@ -0,0 +1,40 @@
+import { UsernamePasswordInput } from "src/resolvers/UsernamePasswordInput";
+
+export const validateRegister = (options: UsernamePasswordInput) => {
+	if (!options.email.includes("@")) {
+		return [
+			{
+				field: "email",
+				message: "Invalid email",
+			},
+		];
+	}
+	if (options.username.length <= 2) {
+		return [
+			{
+				field: "username",
+				message: "Username length must be greater than 2",
+			},
+		];
+	}
+	// we want to allow users to login with their email, so we can't have @ signs in their usernames
+	// we need to be able to differentiate between emails and usernames
+	if (options.username.includes("@")) {
+		return [
+			{
+				field: "username",
+				message: "Invalid symbol '@' in username",
+			},
+		];
+	}
+
+	if (options.password.length <= 3) {
+		return [
+			{
+				field: "password",
+				message: "Password length must be greater than 3",
+			},
+		];
+	}
+	return null;
+};
