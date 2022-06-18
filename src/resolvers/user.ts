946fcdb8b81a2193a6ec743ed318a2aa3efa8a52
Youngjun <yjl.2000@hotmail.com>
2022-06-18 02:04:41 -0400

    modified forgot password email with HTML templating

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -285,19 +285,223 @@ export class UserResolver {
 			user.firstName[0].toUpperCase() + user.firstName.slice(1);
 		await sendEmail(
 			email,
-			"PTC Coding Coding Challenge - Account Password Reset",
-			`<html>
-				<head>
-					<style>
-					</style>
-				</head>
-				<body>
-					<p>Hi ${username},</p>
-					<p>You requested to reset your password.</p>
-					<p> Please click the link below to reset your password.</p>
-					<a href="http://localhost:3000/change-password/${token}">Reset PTC account password</a>
-				</body>
-			</html>`
+			"PTC Coding Challenge - Account Password Reset",
+
+			`<!DOCTYPE html>
+			<html>
+			<head>
+			<title></title>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1">
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+			<style type="text/css">    
+				/* CLIENT-SPECIFIC STYLES */
+				body, table, td, a { -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
+				table, td { mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
+				img { -ms-interpolation-mode: bicubic; }
+			
+				/* RESET STYLES */
+				img { border: 0; height: auto; line-height: 100%; outline: none; text-decoration: none; }
+				table { border-collapse: collapse !important; }
+				body { height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important; }
+			
+				/* iOS BLUE LINKS */
+				a[x-apple-data-detectors] {
+					color: inherit !important;
+					text-decoration: none !important;
+					font-size: inherit !important;
+					font-family: inherit !important;
+					font-weight: inherit !important;
+					line-height: inherit !important;
+				}
+				
+				/* MOBILE STYLES */
+				@media screen and (max-width:600px){
+					h1 {
+						font-size: 32px !important;
+						line-height: 32px !important;
+					}
+				}
+			
+				/* ANDROID CENTER FIX */
+				div[style*="margin: 16px 0;"] { margin: 0 !important; }
+			</style>
+			
+			<style type="text/css">
+			
+			</style>
+			</head>
+			<body style="background-color: #f4f4f4; margin: 0 !important; padding: 0 !important;">
+			
+			<!-- HIDDEN PREHEADER TEXT -->
+			<div style="display: none; font-size: 1px; color: #fefefe; line-height: 1px; font-family: Helvetica, Arial, sans-serif; max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden;">
+				Reset your password
+			</div>
+			
+			<table border="0" cellpadding="0" cellspacing="0" width="100%">
+				<!-- LOGO -->
+				<tr>
+					<td bgcolor="#f4f4f4" align="center">
+						<!--[if (gte mso 9)|(IE)]>
+						<table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
+						<tr>
+						<td align="center" valign="top" width="600">
+						<![endif]-->
+						<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
+							<tr>
+								<td align="center" valign="top" style="padding: 40px 10px 40px 10px;">
+									<a href="http://rightindem.com" target="_blank">
+										<img alt="Logo" src="https://rightindem.com/wp-content/themes/HTML5-Reset-WordPress-Theme-master/images/logo.png" width="169" height="40" style="display: block; width: 169px; max-width: 169px; min-width: 169px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; font-size: 18px;" border="0">
+									</a>
+								</td>
+							</tr>
+						</table>
+						<!--[if (gte mso 9)|(IE)]>
+						</td>
+						</tr>
+						</table>
+						<![endif]-->
+					</td>
+				</tr>
+				<!-- HERO -->
+				<tr>
+					<td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
+						<!--[if (gte mso 9)|(IE)]>
+						<table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
+						<tr>
+						<td align="center" valign="top" width="600">
+						<![endif]-->
+						<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
+							<tr>
+								<td bgcolor="#ffffff" align="center" valign="top" style="padding: 40px 20px 20px 20px; border-radius: 4px 4px 0px 0px; color: #111111; font-family: Helvetica, Arial, sans-serif; font-size: 48px; font-weight: 400; letter-spacing: 4px; line-height: 48px;">
+								  <h1 style="font-size: 28px; font-weight: 400; margin: 0; letter-spacing: 0px;">Reset your password</h1>
+								</td>
+							</tr>
+						</table>
+						<!--[if (gte mso 9)|(IE)]>
+						</td>
+						</tr>
+						</table>
+						<![endif]-->
+					</td>
+				</tr>
+				<!-- COPY BLOCK -->
+				<tr>
+					<td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
+						<!--[if (gte mso 9)|(IE)]>
+						<table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
+						<tr>
+						<td align="center" valign="top" width="600">
+						<![endif]-->
+						<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
+						  <!-- COPY -->
+						  <tr>
+							<td bgcolor="#ffffff" align="left" style="padding: 20px 30px 40px 30px; color: #666666; font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;" >
+							  <p style="margin: 0;">Hello ${username},</p> 
+							  <p style="margin: 0;">We've received a request to reset the password for the PTC account associated with: ${email}.</p>
+							  <p style="margin: 0;">You can reset your password by clicking the link below:</p>
+							  
+							</td>
+						  </tr>
+						  <!-- BULLETPROOF BUTTON -->
+						  <tr>
+							<td bgcolor="#ffffff" align="left">
+							  <table width="100%" border="0" cellspacing="0" cellpadding="0">
+								<tr>
+								  <td bgcolor="#ffffff" align="center" style="padding: 20px 30px 60px 30px;">
+									<table border="0" cellspacing="0" cellpadding="0">
+									  <tr>
+										  <td align="center" style="border-radius: 3px;" bgcolor="#4A35EA"><a href="http://localhost:3000/change-password/${token}" target="_blank" style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid #4A35EA; display: inline-block;">Reset Password</a></td>
+									  </tr>
+									</table>
+								  </td>
+								</tr>
+							  </table>
+							</td>
+						  </tr>
+						  <!-- COPY -->
+						  <tr>
+							<td bgcolor="#ffffff" align="left" style="padding: 0px 30px 0px 30px; color: #666666; font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;" >
+							  <p style="margin: 0;">If that doesn't work, copy and paste the following link in your browser:</p>
+							</td>
+						  </tr>
+						  <!-- COPY -->
+							<tr>
+							  <td bgcolor="#ffffff" align="left" style="padding: 20px 30px 20px 30px; color: #666666; font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;" >
+								<p style="margin: 0;"><a href="http://localhost:3000/change-password/${token}" target="_blank" style="color: #4A35EA;">http://localhost:3000/change-password/${token}</a></p>
+							  </td>
+							</tr>
+						  <!-- COPY -->
+						  <tr>
+							<td bgcolor="#ffffff" align="left" style="padding: 0px 30px 20px 30px; color: #666666; font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;" >
+							  <p style="margin: 0;">If you have any questions, just reply to this email—we're always happy to help out.</p>
+							</td>
+						  </tr>
+						  <!-- COPY -->
+						  <tr>
+							<td bgcolor="#ffffff" align="left" style="padding: 0px 30px 40px 30px; border-radius: 0px 0px 4px 4px; color: #666666; font-family: Helvetica, Arial, sans-serif; font-size: 18px; font-weight: 400; line-height: 25px;" >
+							  <p style="margin: 0;">Cheers,<br>The PTC Coding Challenge Team</p>
+							</td>
+						  </tr>
+						</table>
+						<!--[if (gte mso 9)|(IE)]>
+						</td>
+						</tr>
+						</table>
+						<![endif]-->
+					</td>
+				</tr>
+				<!-- FOOTER -->
+				<tr>
+					<td bgcolor="#f4f4f4" align="center" style="padding: 0px 10px 0px 10px;">
+						<!--[if (gte mso 9)|(IE)]>
+						<table align="center" border="0" cellspacing="0" cellpadding="0" width="600">
+						<tr>
+						<td align="center" valign="top" width="600">
+						<![endif]-->
+						<table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;" >
+						  <!-- NAVIGATION -->
+						  <tr>
+							<td bgcolor="#f4f4f4" align="left" style="padding: 30px 30px 30px 30px; color: #666666; font-family: Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;" >
+							  <p style="margin: 0;">
+								<a href="https://www.projecttechconferences.com/" target="_blank" style="color: #111111; font-weight: 700;">PTC Website</a> -
+								<a href="http://localhost:3000" target="_blank" style="color: #111111; font-weight: 700;">PTC Coding Challenge</a> -
+								<a href="https://www.projecttechconferences.com/contact" target="_blank" style="color: #111111; font-weight: 700;">Contact Us</a>
+							  </p>
+							</td>
+						  </tr>
+						  <!-- ADDRESS -->
+						  <tr>
+							<td bgcolor="#f4f4f4" align="left" style="padding: 0px 30px 30px 30px; color: #666666; font-family: Helvetica, Arial, sans-serif; font-size: 14px; font-weight: 400; line-height: 18px;" >
+							  <p style="margin: 0;">Charitable Registration No. 771791670 RR 0001</p>
+							</td>
+						  </tr>
+						</table>
+						<!--[if (gte mso 9)|(IE)]>
+						</td>
+						</tr>
+						</table>
+						<![endif]-->
+					</td>
+				</tr>
+			</table>
+				
+			</body>
+			</html>
+			`
+			// `<html>
+			// 	<head>
+			// 		<style>
+			// 		</style>
+			// 	</head>
+			// 	<body>
+			// 		<p>Hello ${username},</p>
+			// 		<p>We've received a request to reset the password for the PTC account associated with ${email}.</p>
+			// 		<p> You can reset your password by clicking the link below:</p>
+			// 		<button>Reset your password					<a href="http://localhost:3000/change-password/${token}">Reset PTC account password</a>
+			// 		</button>
+			// 	</body>
+			// </html>`
 		);
 
 		return {
