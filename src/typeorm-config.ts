83a2427bf4fae38712d57c1ecfd04abcd48b8f41
Youngjun <yjl.2000@hotmail.com>
2022-07-03 03:01:00 -0400

    heroku datasource for psql

--- src/typeorm-config.ts
+++ src/typeorm-config.ts
@@ -7,12 +7,28 @@ import { Submissions } from "./entities/Submissions";
 
 require("dotenv").config();
 
+// export const AppDataSource = new DataSource({
+// 	type: "postgres",
+// 	database: process.env.DATABASE_NAME,
+// 	username: process.env.USERNAME,
+// 	password: process.env.PASSWORD,
+// 	synchronize: true,
+// 	migrations: [path.join(__dirname, "./migrations/*")],
+// 	entities: [User, Submissions],
+// });
+
+// prod
 export const AppDataSource = new DataSource({
 	type: "postgres",
-	database: process.env.DATABASE_NAME,
-	username: process.env.USERNAME,
-	password: process.env.PASSWORD,
+	// database: process.env.HEROKU_DATABASE,
+	// username: process.env.HEROKU_USERNAME,
+	// password: process.env.HEROKU_PASSWORD,
+	// host: process.env.HEROKU_HOST,
+	url: process.env.DATABASE_URL,
 	synchronize: true,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 	migrations: [path.join(__dirname, "./migrations/*")],
 	entities: [User, Submissions],
 });
